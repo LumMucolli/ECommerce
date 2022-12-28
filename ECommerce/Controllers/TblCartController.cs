981e4ef74fb932962c186c9a5dcea0dc4a68485c
LumMucolli <lm51179@ubt-uni.net>
2022-12-28 08:49:49 +0100

    Created view for Product

---
+++ ECommerce/Controllers/TblCartController.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ECommerce.Database;
+
+namespace ECommerce.Controllers
+{
+    public class TblCartController : Controller
+    {
+        private readonly EcommerceContext _context;
+
+        public TblCartController(EcommerceContext context)
+        {
+            _context = context;
+        }
+
+        // GET: TblCart
+        public async Task<IActionResult> Index()
+        {
+            var ecommerceContext = _context.TblCarts.Include(t => t.Product);
+            return View(await ecommerceContext.ToListAsync());
+        }
+
+        // GET: TblCart/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.TblCarts == null)
+            {
+                return NotFound();
+            }
+
+            var tblCart = await _context.TblCarts
+                .Include(t => t.Product)
+                .FirstOrDefaultAsync(m => m.CartId == id);
+            if (tblCart == null)
+            {
+                return NotFound();
+            }
+
+            return View(tblCart);
+        }
+
+        // GET: TblCart/Create
+        public IActionResult Create()
+        {
+            ViewData["ProductId"] = new SelectList(_context.TblProducts, "ProductId", "ProductId");
+            return View();
+        }
+
+        // POST: TblCart/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("CartId,ProductId,MemberId,CartStatusId")] TblCart tblCart)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(tblCart);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ProductId"] = new SelectList(_context.TblProducts, "ProductId", "ProductId", tblCart.ProductId);
+            return View(tblCart);
+        }
+
+        // GET: TblCart/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.TblCarts == null)
+            {
+                return NotFound();
+            }
+
+            var tblCart = await _context.TblCarts.FindAsync(id);
+            if (tblCart == null)
+            {
+                return NotFound();
+            }
+            ViewData["ProductId"] = new SelectList(_context.TblProducts, "ProductId", "ProductId", tblCart.ProductId);
+            return View(tblCart);
+        }
+
+        // POST: TblCart/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("CartId,ProductId,MemberId,CartStatusId")] TblCart tblCart)
+        {
+            if (id != tblCart.CartId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(tblCart);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!TblCartExists(tblCart.CartId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["ProductId"] = new SelectList(_context.TblProducts, "ProductId", "ProductId", tblCart.ProductId);
+            return View(tblCart);
+        }
+
+        // GET: TblCart/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.TblCarts == null)
+            {
+                return NotFound();
+            }
+
+            var tblCart = await _context.TblCarts
+                .Include(t => t.Product)
+                .FirstOrDefaultAsync(m => m.CartId == id);
+            if (tblCart == null)
+            {
+                return NotFound();
+            }
+
+            return View(tblCart);
+        }
+
+        // POST: TblCart/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.TblCarts == null)
+            {
+                return Problem("Entity set 'EcommerceContext.TblCarts'  is null.");
+            }
+            var tblCart = await _context.TblCarts.FindAsync(id);
+            if (tblCart != null)
+            {
+                _context.TblCarts.Remove(tblCart);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool TblCartExists(int id)
+        {
+          return _context.TblCarts.Any(e => e.CartId == id);
+        }
+    }
+}
---
+++ ECommerce/Controllers/TblProductController.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ECommerce.Database;
+
+namespace ECommerce.Controllers
+{
+    public class TblProductController : Controller
+    {
+        private readonly EcommerceContext _context;
+
+        public TblProductController(EcommerceContext context)
+        {
+            _context = context;
+        }
+
+        // GET: TblProduct
+        public async Task<IActionResult> Index()
+        {
+            var ecommerceContext = _context.TblProducts.Include(t => t.Category);
+            return View(await ecommerceContext.ToListAsync());
+        }
+
+        // GET: TblProduct/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.TblProducts == null)
+            {
+                return NotFound();
+            }
+
+            var tblProduct = await _context.TblProducts
+                .Include(t => t.Category)
+                .FirstOrDefaultAsync(m => m.ProductId == id);
+            if (tblProduct == null)
+            {
+                return NotFound();
+            }
+
+            return View(tblProduct);
+        }
+
+        // GET: TblProduct/Create
+        public IActionResult Create()
+        {
+            ViewData["CategoryId"] = new SelectList(_context.TblCategories, "CategoryId", "CategoryId");
+            return View();
+        }
+
+        // POST: TblProduct/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("ProductId,ProductName,CategoryId,IsActive,IsDelete,CreatedDate,ModifiedDate,Description,ProductImage,IsFeatured,Quantity")] TblProduct tblProduct)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(tblProduct);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CategoryId"] = new SelectList(_context.TblCategories, "CategoryId", "CategoryId", tblProduct.CategoryId);
+            return View(tblProduct);
+        }
+
+        // GET: TblProduct/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.TblProducts == null)
+            {
+                return NotFound();
+            }
+
+            var tblProduct = await _context.TblProducts.FindAsync(id);
+            if (tblProduct == null)
+            {
+                return NotFound();
+            }
+            ViewData["CategoryId"] = new SelectList(_context.TblCategories, "CategoryId", "CategoryId", tblProduct.CategoryId);
+            return View(tblProduct);
+        }
+
+        // POST: TblProduct/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("ProductId,ProductName,CategoryId,IsActive,IsDelete,CreatedDate,ModifiedDate,Description,ProductImage,IsFeatured,Quantity")] TblProduct tblProduct)
+        {
+            if (id != tblProduct.ProductId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(tblProduct);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!TblProductExists(tblProduct.ProductId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CategoryId"] = new SelectList(_context.TblCategories, "CategoryId", "CategoryId", tblProduct.CategoryId);
+            return View(tblProduct);
+        }
+
+        // GET: TblProduct/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.TblProducts == null)
+            {
+                return NotFound();
+            }
+
+            var tblProduct = await _context.TblProducts
+                .Include(t => t.Category)
+                .FirstOrDefaultAsync(m => m.ProductId == id);
+            if (tblProduct == null)
+            {
+                return NotFound();
+            }
+
+            return View(tblProduct);
+        }
+
+        // POST: TblProduct/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.TblProducts == null)
+            {
+                return Problem("Entity set 'EcommerceContext.TblProducts'  is null.");
+            }
+            var tblProduct = await _context.TblProducts.FindAsync(id);
+            if (tblProduct != null)
+            {
+                _context.TblProducts.Remove(tblProduct);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool TblProductExists(int id)
+        {
+          return _context.TblProducts.Any(e => e.ProductId == id);
+        }
+    }
+}
